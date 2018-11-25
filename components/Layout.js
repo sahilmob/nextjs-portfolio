ac4ac9b7d249c3811c742c0e5fa66cc3e3f4c6e7
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-11-25 22:45:37 +0200

    add loading bar on page transition

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,26 @@
 import Link from "next/link";
+import Head from "next/head";
+import Router from "next/router";
+import NProgress from "nprogress";
+
+Router.onRouteChangeStart = url => {
+	console.log(url);
+	NProgress.start();
+};
+
+Router.onRouteChangeComplete = () => NProgress.done();
+Router.onRouteChangeError = () => NProgress.done();
 
 export default function Layout({ children, title }) {
 	return (
 		<div className="root">
+			<Head>
+				<title>Next Portfolio</title>
+				<link
+					rel="stylesheet"
+					href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
+				/>
+			</Head>
 			<header>
 				<Link href="/">
 					<a>Home</a>
