ba03559476658d948b5cd41afece3bf76f5cad41
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-11-25 22:11:52 +0200

    add header links

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,19 @@
+import Link from "next/link";
+
 export default function Layout({ children, title }) {
 	return (
 		<div>
-			<header>header</header>
+			<header>
+				<Link href="/">
+					<a>Home</a>
+				</Link>
+				<Link href="/about">
+					<a>About</a>
+				</Link>
+				<Link href="/hireme">
+					<a>Hire Me</a>
+				</Link>
+			</header>
 			<h1>{title}</h1>
 			{children}
 			<footer>footer</footer>
