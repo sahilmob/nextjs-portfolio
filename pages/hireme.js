efb4ff0003b1994bd92ea2e418159937fc479607
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-11-25 22:00:52 +0200

    create hire me page

---
+++ pages/hireme.js
@@ -0,0 +1,11 @@
+export default function Hireme() {
+	return (
+		<div>
+			<h1>Hire me</h1>
+			<p>
+				You can hire me at{" "}
+				<a href="sahil.hmob@hotmail.com">sahil.hmob@hotmail.com</a>
+			</p>
+		</div>
+	);
+}
