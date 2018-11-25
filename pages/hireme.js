29c6449e966c7672f4a87fa7cb268a1b2280c66a
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-11-25 22:16:26 +0200

    add layout component

--- pages/hireme.js
+++ pages/hireme.js
@@ -1,11 +1,12 @@
+import Layout from "../components/Layout";
 export default function Hireme() {
 	return (
-		<div>
+		<Layout title="Hire Me">
 			<h1>Hire me</h1>
 			<p>
 				You can hire me at{" "}
 				<a href="sahil.hmob@hotmail.com">sahil.hmob@hotmail.com</a>
 			</p>
-		</div>
+		</Layout>
 	);
 }
