ea90323ffeefb9b4a1dafb38ff589c1226e6ccde
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-11-25 22:10:25 +0200

    get title from props

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,8 @@
-export default function Layout({ children }) {
+export default function Layout({ children, title }) {
 	return (
 		<div>
 			<header>header</header>
+			<h1>{title}</h1>
 			{children}
 			<footer>footer</footer>
 		</div>
