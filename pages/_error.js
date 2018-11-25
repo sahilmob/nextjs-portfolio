6f3648a7f2829560110e15183804ad268f4e3d2a
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-11-25 23:37:16 +0200

    handle user data fetching error

--- pages/_error.js
+++ pages/_error.js
@@ -1,9 +1,11 @@
 import Layout from "../components/Layout";
 
-export default function _error() {
+export default function _error({ statusCode }) {
 	return (
 		<Layout title="Error">
-			<p>Couldn't get that page, sorry!</p>
+			{statusCode
+				? `Could not load your user data: status code ${statusCode}`
+				: `Couldn't get that page, sorry!`}
 		</Layout>
 	);
 }
--- pages/about.js
+++ pages/about.js
@@ -2,15 +2,20 @@ import React, { Component } from "react";
 import fetch from "isomorphic-unfetch";
 
 import Layout from "../components/Layout";
+import Error from "./_error";
 export default class about extends Component {
 	static async getInitialProps() {
 		const res = await fetch("http://api.github.com/users/sahilmob");
+		const statusCode = res.status > 200 ? res.status : false;
 		const data = await res.json();
-		return { user: data };
+		return { user: data, statusCode };
 	}
 
 	render() {
-		const { user } = this.props;
+		const { user, statusCode } = this.props;
+		if (statusCode) {
+			return <Error statusCode={statusCode} />;
+		}
 		return (
 			<Layout title="About">
 				<p>{user.name}</p>
