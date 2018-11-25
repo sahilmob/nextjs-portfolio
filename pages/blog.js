d17e2aa4c4c141f8f626f2196b338bca684e8739
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-11-25 23:52:52 +0200

    use slug for url

--- pages/blog.js
+++ pages/blog.js
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 import Layout from "../components/Layout";
 
-const PostLink = ({ title }) => (
+const PostLink = ({ title, slug }) => (
 	<li>
-		<Link href={`/post?title=${title}`}>
+		<Link href={`/post?title=${title}`} as={`/${slug}`}>
 			<a>{title}</a>
 		</Link>
 	</li>
@@ -14,9 +14,9 @@ export default function blog() {
 	return (
 		<Layout title="My Blog">
 			<ul>
-				<PostLink title="react" />
-				<PostLink title="angular" />
-				<PostLink title="vue" />
+				<PostLink slug="react-post" title="React Post" />
+				<PostLink slug="angular-post" title="Angular Post" />
+				<PostLink slug="vue-post" title="Vue Post" />
 			</ul>
 		</Layout>
 	);
