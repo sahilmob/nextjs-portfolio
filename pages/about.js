9c62a1919bcca841679500e4b34495ec57752615
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-11-25 20:31:54 +0200

    add navigation links

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function About() {
 	return (
 		<div>
 			<h1>About</h1>
+			<Link href="/">
+				<a>Go to home</a>
+			</Link>
 			<p>A javascript programmer</p>
 			<img src="/static/JavaScript-logo.png" alt="javascript" height="200px" />
 		</div>
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
+import Link from "next/link";
+
 export default function Index() {
 	return (
 		<div>
 			<h1>Home</h1>
-			Welcome to the homepage
+			<Link href="/about">
+				<a>Go to about</a>
+			</Link>
+			<p>Welcome to the homepage</p>
 		</div>
 	);
 }
