a143c7b67f367ebea1f276cfaf6062a50b695325
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-11-25 22:28:49 +0200

    general styling

--- components/Layout.js
+++ components/Layout.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Layout({ children, title }) {
 	return (
-		<div>
+		<div className="root">
 			<header>
 				<Link href="/">
 					<a>Home</a>
@@ -17,6 +17,44 @@ export default function Layout({ children, title }) {
 			<h1>{title}</h1>
 			{children}
 			<footer>&copy; {new Date().getFullYear()}</footer>
+			<style jsx>
+				{`
+					.root {
+						display: flex;
+						justify-content: center;
+						align-items: center;
+						flex-direction: column;
+					}
+					header {
+						width: 100%;
+						display: flex;
+						justify-content: space-around;
+						padding: 1em;
+						font-size: 1.2rem;
+						background: indigo;
+					}
+					header a {
+						color: darkgrey;
+						text-decoration: none;
+					}
+					header a:hove {
+						font-weight: bold;
+						color: lightgrey;
+					}
+					footer {
+						padding: 1em;
+					}
+				`}
+			</style>
+			<style jsx global>
+				{`
+					body {
+						margin: 0;
+						font-size: 110%;
+						background: #f0f0f0;
+					}
+				`}
+			</style>
 		</div>
 	);
 }
