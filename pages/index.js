d0c773e5c528cd025fac33c4cd83db297ff72101
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-11-25 22:09:58 +0200

    add layout component

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import Link from "next/link";
+import Layout from "../components/Layout";
 
 export default function Index() {
 	return (
-		<div>
+		<Layout title="Home">
 			<h1>Home</h1>
 			<Link href="/about">
 				<a>Go to about</a>
 			</Link>
 			<p>Welcome to the homepage</p>
-		</div>
+		</Layout>
 	);
 }
